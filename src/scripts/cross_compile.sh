444fefa2239fe1f1cf137ebb842031d38cd89652
joerg kost <joerg.kost@gmx.com>
2017-03-03 18:04:19 +0100

    Crosscompiled new versions for linux,mac, also windows.

--- src/scripts/cross_compile.sh
+++ src/scripts/cross_compile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cd ..
 env GOOS=linux GOARCH=amd64 go build -o bin/asnbuilder.linux main.go
+env GOOS=windows GOARCH=amd64 go  build -o bin/asnbuilder.exe main.go
+env GOOS=darwin GOARCH=amd64 go  build -o bin/asnbuilder.mac main.go
